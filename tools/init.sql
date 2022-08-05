762732ce99b4a9d28c110d2e8465be5e51532a39
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 11:20:33 +0530

    Add assets_path column
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/init.sql
+++ tools/init.sql
@@ -3,15 +3,21 @@
 -- identified by a combination of its tag and version. However, we use a
 -- separate integer primary for performance reasons. Templates may be archived,
 -- i.e. they are soft-deleted, and the is_archived flag tracks this. The actual
--- template content is stored in the content field. We choose to abort in case
--- there is a unique constraint violation; this is the default behaviour but we
--- are making it explicit.
+-- template content is stored in the content field.
+--
+-- The assets_path is the path to the assets directory; ideally this should be
+-- a zip file with all the necessary data, but it is more involved and so we're
+-- deferring this feature for a later time.
+--
+-- We choose to abort in case there is a unique constraint violation; this is
+-- the default behaviour but we are making it explicit.
 
 CREATE TABLE IF NOT EXISTS templates (
        id               INTEGER PRIMARY KEY,
        tag              TEXT NOT NULL,
        version          TEXT NOT NULL,
        content          TEXT NOT NULL,
+       assets_path      TEXT,
        is_archived      INTEGER NOT NULL DEFAULT 0,
        UNIQUE           (id, tag) ON CONFLICT ABORT
 );
