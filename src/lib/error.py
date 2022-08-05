32ee9ae815b206fb235126f6cabfdf6c0b8acec0
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 18:30:36 +0530

    Define error class
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ src/lib/error.py
@@ -0,0 +1,40 @@
+
+
+class Error:
+    """Represents a processing error. """
+
+    def __init__(self, code, rule, msg):
+        """Initialises this error. """
+        self._code = code
+        self._rule = rule
+        self._msg = msg
+
+    @property
+    def code(self):
+        """Gets the error code."""
+        return self._code
+
+    @code.setter
+    def code(self, value):
+        """Sets the error code."""
+        self._code = value
+
+    @property
+    def rule(self):
+        """Gets the rule where the error occurred."""
+        return self._rule
+
+    @rule.setter
+    def rule(self, value):
+        """Sets the rule where the error occurred."""
+        self._rule = value
+
+    @property
+    def msg(self):
+        """Gets the error message."""
+        return self._msg
+
+    @msg.setter
+    def msg(self, value):
+        """Sets the error message."""
+        self._msg = value
