ff9a65e806ba7c6b16a7386d09cceaa1f92e7071
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 16:00:55 +0530

    Define remaining tests for archiving template
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tests/features/archive_template.feature
+++ tests/features/archive_template.feature
@@ -9,5 +9,16 @@ Feature: Archiving a template
     Then the template with ID 1 is archived
 
   Scenario: Archiving a non-existent template
+    Given there is no active template with ID 9999
+    When `stupa --id=9999 archive` is invoked
+    Then a not found error occurs
 
   Scenario: Archiving an archived template
+    Given there is an archived template with ID 2
+    When `stupa --id=2 archive` is invoked
+    Then an invalid operation error occurs
+
+  Scenario: Archiving without specifying ID
+    Given there is an active template with ID 1
+    When `stupa archive` is invoked
+    Then a missing flag error occurs
--- tests/step_defs/test_archive_template.py
+++ tests/step_defs/test_archive_template.py
@@ -13,11 +13,46 @@ def check_template_id_1_active():
     pass
 
 
+@given('there is an archived template with ID 2')
+def check_template_id_2_archived():
+    pass
+
+
+@given('there is no active template with ID 9999')
+def check_template_id_9999_does_not_exist():
+    pass
+
+
 @when('`stupa --id=1 archive` is invoked')
 def archive_template_id_1():
     pass
 
 
+@when('`stupa --id=2 archive` is invoked')
+def archive_template_id_2():
+    pass
+
+
+@when('`stupa --id=9999 archive` is invoked')
+def archive_template_id_9999():
+    pass
+
+
 @then('the template with ID 1 is archived')
 def check_template_id_1_archived():
+    pass
+
+
+@then('a not found error occurs')
+def check_not_found_error():
+    pass
+
+
+@then('an invalid operation error occurs')
+def check_invalid_operation_error():
+    pass
+
+
+@then('a missing flag error occurs')
+def check_missing_flag_error():
     pass
