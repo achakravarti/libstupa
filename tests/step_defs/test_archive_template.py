b3de8aaed4005db5fb7db57d3927eee639cbd9b6
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 15:49:26 +0530

    Disable all tests
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tests/step_defs/test_archive_template.py
+++ tests/step_defs/test_archive_template.py
@@ -10,17 +10,14 @@ scenarios('../features/archive_template.feature')
 
 @given('there is an active template with ID 1')
 def check_template_id_1_active():
-    print('check_template_id_1_active')
-    assert 1 == 1
+    pass
 
 
 @when('`stupa --id=1 archive` is invoked')
 def archive_template_id_1():
-    print('archive_template_id_1')
-    assert 1 == 1
+    pass
 
 
 @then('the template with ID 1 is archived')
 def check_template_id_1_archived():
-    print('template with ID 1 is archived')
-    assert 1 == 1
+    pass
