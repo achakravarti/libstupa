780120354dd39ce67097580c71fa14e9447ca75a
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 11:52:01 +0530

    Complete command queries
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/queries.sql
+++ src/queries.sql
@@ -1,8 +1,3 @@
--- update template
--- archive template
--- restore template
--- delete template
---
 -- list templates
 -- list templates csv
 -- load template
@@ -10,4 +5,27 @@
 
 -- create new template
 INSERT INTO templates (tag, version, content, assets_path, is_archived)
-VALUES (?, ?, ?, ?, FALSE);
+VALUES (?, ?, ?, ?, 0);
+
+
+-- update template
+UPDATE templates
+SET tag = ?, version = ?, content = ?, assets_path =?
+WHERE id = ?;
+
+
+-- archive template
+UPDATE templates
+SET is_archived = 1
+WHERE id = ?;
+
+
+-- restore template
+UPDATE templates
+SET is_archived = 0
+WHERE id = ?;
+
+
+-- delete template
+DELETE FROM templates
+WHERE id = ?;
