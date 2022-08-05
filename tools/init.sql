54d058e2fbe0b0b421f5437aab53ec06645b1d48
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 12:01:16 +0530

    Remove assets_path column
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/init.sql
+++ tools/init.sql
@@ -5,10 +5,6 @@
 -- i.e. they are soft-deleted, and the is_archived flag tracks this. The actual
 -- template content is stored in the content field.
 --
--- The assets_path is the path to the assets directory; ideally this should be
--- a zip file with all the necessary data, but it is more involved and so we're
--- deferring this feature for a later time.
---
 -- We choose to abort in case there is a unique constraint violation; this is
 -- the default behaviour but we are making it explicit.
 
@@ -17,7 +13,6 @@ CREATE TABLE IF NOT EXISTS templates (
        tag              TEXT NOT NULL,
        version          TEXT NOT NULL,
        content          TEXT NOT NULL,
-       assets_path      TEXT,
        is_archived      INTEGER NOT NULL DEFAULT 0,
        UNIQUE           (id, tag) ON CONFLICT ABORT
 );
