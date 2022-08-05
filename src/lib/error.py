5b9d2385cd2b4e2f44e8133304b44001b9e24b27
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 18:38:36 +0530

    Add typing hints
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/lib/error.py
+++ src/lib/error.py
@@ -12,38 +12,38 @@ class ErrorCode(enum.Enum):
 class Error:
     """Represents a processing error. """
 
-    def __init__(self, code, rule, msg):
+    def __init__(self, code: ErrorCode, rule: str, msg: str):
         """Initialises this error. """
         self._code = code
         self._rule = rule
         self._msg = msg
 
     @property
-    def code(self):
+    def code(self) -> ErrorCode:
         """Gets the error code."""
         return self._code
 
     @code.setter
-    def code(self, value):
+    def code(self, value: ErrorCode):
         """Sets the error code."""
         self._code = value
 
     @property
-    def rule(self):
+    def rule(self) -> str:
         """Gets the rule where the error occurred."""
         return self._rule
 
     @rule.setter
-    def rule(self, value):
+    def rule(self, value: str):
         """Sets the rule where the error occurred."""
         self._rule = value
 
     @property
-    def msg(self):
+    def msg(self) -> str:
         """Gets the error message."""
         return self._msg
 
     @msg.setter
-    def msg(self, value):
+    def msg(self, value: str):
         """Sets the error message."""
         self._msg = value
