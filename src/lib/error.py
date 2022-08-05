0ec3cdd24bbbb4185925385a2850ac46d045013c
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-05 18:49:29 +0530

    Define error class
    The Error and ErrorCode classes have been defined in order to better
    structure errors returned by the rule functions (which are yet to be
    implemented).
    
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ src/lib/error.py
@@ -0,0 +1,66 @@
+import enum
+import json
+
+
+class ErrorCode(enum.Enum):
+    NONE = 0
+    DATABASE = 1
+    NOTFOUND = 2
+    MISSINGFLAG =3
+    INVALIDOP = 4
+
+
+class Error:
+    """Represents a processing error. """
+
+    def __init__(self, code: ErrorCode, rule: str, msg: str):
+        """Initialises this error. """
+        self._code = code
+        self._rule = rule
+        self._msg = msg
+
+    @property
+    def code(self) -> ErrorCode:
+        """Gets the error code."""
+        return self._code
+
+    @code.setter
+    def code(self, value: ErrorCode):
+        """Sets the error code."""
+        self._code = value
+
+    @property
+    def rule(self):
+        """Gets the rule where the error occurred."""
+        return self._rule
+
+    @rule.setter
+    def rule(self, value):
+        """Sets the rule where the error occurred."""
+        self._rule = value
+
+    @property
+    def msg(self):
+        """Gets the error message."""
+        return self._msg
+
+    @msg.setter
+    def msg(self, value):
+        """Sets the error message."""
+        self._msg = value
+
+
+    def __str__(self):
+        """Gets the string representation of this error."""
+        return 'Code: {}\nRule: {}\nMessage: {}'.format(
+            self.code, self.rule, self.msg)
+
+
+    def json(self):
+        """Gets the JSON representation of this error."""
+        jd = {
+            'code': self.code,
+            'rule': self.rule,
+            'msg': self.msg
+        }
+        return json.dumps(jd, indent=2)
